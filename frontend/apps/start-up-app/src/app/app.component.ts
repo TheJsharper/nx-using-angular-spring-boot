d17251503da877fc549c8cf6369320b4d83377a5
Pedro Salomon <pedro.salomonocampos@atos.net>
2023-09-11 15:48:25 +0200

    initial commit

---
+++ frontend/apps/start-up-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-using-angular-spring-boot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'start-up-app';
+}

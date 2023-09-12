b1490033105df46b22ec42440b64c1e8c88638a1
Pedro Salomon <pedro.salomonocampos@atos.net>
2023-09-12 13:29:35 +0200

    testing frontend and backend

---
+++ backend/apps/start-up-app/src/test/java/com/jsharper/startupapp/StartUpAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsharper.startupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

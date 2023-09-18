854d4476f854868173fa69d6ef5db42b2a98fc0f
Pedro Salomon <pedro.salomonocampos@atos.net>
2023-09-18 08:48:49 +0200

    fixed problem with swagger ui but with warning

--- backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/SpringFoxConfig.java
+++ backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/SpringFoxConfig.java
@@ -1,7 +1,6 @@
 package com.jsharper.startupapp.configurations;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,7 +8,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
+/*@Configuration*/
 @EnableSwagger2
 public class SpringFoxConfig {
 
@@ -17,9 +16,33 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.jsharper"))
-                .paths(PathSelectors.ant("/foos/*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 
 }
+
+//@Configuration
+/*@EnableSwagger2
+public class SpringFoxConfig {
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+				.apiInfo(apiInfo()).select().paths(postPaths()).build();
+	}
+
+	private Predicate<String> postPaths() {
+		return or(regex("/api/posts.*"), regex("/api/javainuse.*"));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("JavaInUse API")
+				.description("JavaInUse API reference for developers")
+				.termsOfServiceUrl("http://javainuse.com")
+				.contact("javainuse@gmail.com").license("JavaInUse License")
+				.licenseUrl("javainuse@gmail.com").version("1.0").build();
+	}
+
+}*/

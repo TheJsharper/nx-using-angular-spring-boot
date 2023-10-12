725d02975aae1c523ccac360b45bad79bfcfc391
Pedro Salomon Ocampos <pedro.salomonocampos@atos.net>
2023-10-12 17:12:05 +0200

    micro testing ui

--- frontend/apps/start-up-app/src/app/app.module.ts
+++ frontend/apps/start-up-app/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
 import { SharedModule } from '@nx-using-angular-spring-boot/shared';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatExpansionModule, BrowserAnimationsModule, MatTableModule, SharedModule],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/libs/src/lib/share/material/components/sticky-table-footer/sticky-table-footer.module.ts
+++ frontend/libs/src/lib/share/material/components/sticky-table-footer/sticky-table-footer.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { LibStickyTableFooterComponent } from './sticky-table-footer.component';
 
+import {MatExpansionModule} from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+
 @NgModule({
-    imports: [],
-    exports: [],
+    
+    imports: [MatExpansionModule, BrowserAnimationsModule, MatTableModule ],
+    exports: [LibStickyTableFooterComponent],
     declarations: [LibStickyTableFooterComponent],
     providers: [],
 })

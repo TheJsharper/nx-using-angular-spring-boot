6f2b98862e633f2d8f4d366efe24fb017175a0a8
Pedro Salomon <pedro.salomon@gmail.com>
2024-03-03 22:33:35 +0100

    suppress all warning

--- backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/MySecurityBaseOnceFilter.java
+++ backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/MySecurityBaseOnceFilter.java
@@ -2,6 +2,7 @@ package com.jsharper.startupapp.configurations;
 
 import java.io.IOException;
 
+import org.springframework.lang.NonNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -12,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class MySecurityBaseOnceFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
     
                 System.out.println("OncePerRequestFilter Before");
--- backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/WebConfig.java
+++ backend/apps/start-up-app/src/main/java/com/jsharper/startupapp/configurations/WebConfig.java
@@ -1,6 +1,7 @@
 package com.jsharper.startupapp.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 

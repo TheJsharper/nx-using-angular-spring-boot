286ec0d458e6c1a67d6249f0680cde5e32518627
Pedro Salomon <pedro.salomon@gmail.com>
2024-03-12 23:03:59 +0100

    lib form added

---
+++ frontend/libs/form/src/lib/form/form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-using-angular-spring-boot-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent {}
